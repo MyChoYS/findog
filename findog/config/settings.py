ab933b3f74cfe7e8b1fc29d7cf7db32dc1723a6a
Cho - Yong Sang <76713912+MyChoYS@users.noreply.github.com>
2021-05-20 14:38:36 +0900

    findog database

--- findog/config/settings.py
+++ findog/config/settings.py
@@ -77,11 +77,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edu09db',
+        'USER': 'edu09',
+        'PASSWORD': 'multi1234!',
+        'HOST' : 'database-big-a.cljkqcsbb9ok.ap-northeast-2.rds.amazonaws.com',
+        'PORT':3306,
+        'CHARSET' : 'utf-8',
+        'OPTIONS': {
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -123,6 +130,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- findog/findog/models.py
+++ findog/findog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class dogbreed(models.Model): #견종
+    dogbreed = models.CharField(max_length=100,primary_key=True)
+
+class member(models.Model): #회원관리
+    id = models.CharField(max_length=50,primary_key=True)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    home = models.TextField()
+    cellphone = models.CharField(max_length=50)
+
+class mydog(models.Model):
+    member_id = models.ForeignKey(member,on_delete=models.CASCADE)
+    dogbreed = models.ForeignKey(dogbreed,on_delete=models.CASCADE)
+    dogimage = models.ImageField(blank=True, upload_to="photo_%Y_%m_%d")
+    dogname = models.CharField(max_length=50)
+    dogage = models.IntegerField()
+    dogsex = models.CharField(max_length=50)
+
+class missingdog(models.Model): #유기견리스트
+    member_id = models.ForeignKey(member, on_delete=models.CASCADE)
+    dogbreed = models.ForeignKey(dogbreed,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    findplace = models.TextField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+    cellphone = models.IntegerField()
+    explanation = models.TextField()
+    img = models.ImageField(blank=True, upload_to="photo_%Y_%m_%d")
+    findtime = models.DateTimeField()
